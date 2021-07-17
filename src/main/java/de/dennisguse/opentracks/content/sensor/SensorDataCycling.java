d672ad8531b3cde7155ab1cf58d9fe055eb10f82
G Soros <gsoros@users.noreply.github.com>
2021-07-17 21:52:50 +0200

    fixes for cadence sensor

--- src/main/java/de/dennisguse/opentracks/content/sensor/SensorDataCycling.java
+++ src/main/java/de/dennisguse/opentracks/content/sensor/SensorDataCycling.java
@@ -14,7 +14,8 @@ import de.dennisguse.opentracks.util.UintUtils;
 import de.dennisguse.opentracks.util.UnitConversions;
 
 /**
- * Provides cadence in rpm and speed in milliseconds from Bluetooth LE Cycling Cadence and Speed sensors.
+ * Provides cadence in rpm and speed in milliseconds from Bluetooth LE Cycling
+ * Cadence and Speed sensors.
  */
 public final class SensorDataCycling {
 
@@ -25,7 +26,7 @@ public final class SensorDataCycling {
 
     public static class Cadence extends SensorData<Float> {
 
-        private final Long crankRevolutionsCount; // UINT32
+        private final Integer crankRevolutionsCount; // UINT16
         private final Integer crankRevolutionsTime; // UINT16; 1/1024s
 
         public Cadence(String sensorAddress) {
@@ -34,7 +35,7 @@ public final class SensorDataCycling {
             this.crankRevolutionsTime = null;
         }
 
-        public Cadence(String sensorAddress, String sensorName, long crankRevolutionsCount, int crankRevolutionsTime) {
+        public Cadence(String sensorAddress, String sensorName, int crankRevolutionsCount, int crankRevolutionsTime) {
             super(sensorAddress, sensorName);
             this.crankRevolutionsCount = crankRevolutionsCount;
             this.crankRevolutionsTime = crankRevolutionsTime;
@@ -44,14 +45,15 @@ public final class SensorDataCycling {
          * Workaround for Wahoo CADENCE: provides speed instead of cadence
          */
         public Cadence(@NonNull DistanceSpeed speed) {
-            this(speed.getSensorAddress(), speed.getSensorName(), speed.wheelRevolutionsCount, speed.wheelRevolutionsTime);
+            this(speed.getSensorAddress(), speed.getSensorName(), speed.wheelRevolutionsCount.intValue(),
+                    speed.wheelRevolutionsTime);
         }
 
         public boolean hasData() {
             return crankRevolutionsCount != null && crankRevolutionsTime != null;
         }
 
-        public long getCrankRevolutionsCount() {
+        public int getCrankRevolutionsCount() {
             return crankRevolutionsCount;
         }
 
@@ -60,32 +62,45 @@ public final class SensorDataCycling {
         }
 
         public void compute(Cadence previous) {
-            if (hasData() && previous != null && previous.hasData()) {
-                float timeDiff_ms = UintUtils.diff(crankRevolutionsTime, previous.crankRevolutionsTime, UintUtils.UINT16_MAX) / 1024f * UnitConversions.S_TO_MS;
-                if (timeDiff_ms <= 0) {
-                    Log.e(TAG, "Timestamps difference is invalid: cannot compute cadence.");
-                    value = null;
-                } else {
-                    long crankDiff = UintUtils.diff(crankRevolutionsCount, previous.crankRevolutionsCount, UintUtils.UINT32_MAX);
-                    float cadence_ms = crankDiff / timeDiff_ms;
-                    value = (float) (cadence_ms / UnitConversions.MS_TO_S / UnitConversions.S_TO_MIN);
-                }
+            if (!hasData() || previous == null || !previous.hasData()) {
+                Log.e(TAG, "Missing data: cannot compute cadence.");
+                value = null;
+                return;
             }
+            float timeDiff_ms = UintUtils.diff(crankRevolutionsTime, previous.crankRevolutionsTime,
+                    UintUtils.UINT16_MAX) / 1024f * UnitConversions.S_TO_MS;
+            if (timeDiff_ms <= 0) {
+                Log.e(TAG, "Timestamps difference is invalid: cannot compute cadence.");
+                value = previous.value;
+                return;
+            }
+            int crankDiff = (int)UintUtils.diff(crankRevolutionsCount, previous.crankRevolutionsCount, UintUtils.UINT16_MAX);
+            if (crankDiff <= 0) {
+                Log.e(TAG, "Crank revolution counter difference is invalid: cannot compute cadence.");
+                value = previous.value;
+                return;
+            }
+            float cadence_ms = crankDiff / timeDiff_ms;
+            value = (float) (cadence_ms / UnitConversions.MS_TO_S / UnitConversions.S_TO_MIN);
+
         }
 
         @NonNull
         @Override
         public String toString() {
-            return super.toString() + " cadence=" + value + " time=" + crankRevolutionsTime + " count=" + crankRevolutionsCount;
+            return super.toString() + " cadence=" + value + " time=" + crankRevolutionsTime + " count="
+                    + crankRevolutionsCount;
         }
 
         @Override
         public boolean equals(@Nullable Object obj) {
-            if (!(obj instanceof Cadence)) return false;
+            if (!(obj instanceof Cadence))
+                return false;
 
             Cadence comp = (Cadence) obj;
             if (hasData() && comp.hasData() == hasData()) {
-                return getCrankRevolutionsCount() == comp.getCrankRevolutionsCount() && getCrankRevolutionsTime() == comp.getCrankRevolutionsTime();
+                return getCrankRevolutionsCount() == comp.getCrankRevolutionsCount()
+                        && getCrankRevolutionsTime() == comp.getCrankRevolutionsTime();
             } else {
                 return false;
             }
@@ -103,7 +118,8 @@ public final class SensorDataCycling {
             this.wheelRevolutionsTime = null;
         }
 
-        public DistanceSpeed(String sensorAddress, String sensorName, long wheelRevolutionsCount, int wheelRevolutionsTime) {
+        public DistanceSpeed(String sensorAddress, String sensorName, long wheelRevolutionsCount,
+                int wheelRevolutionsTime) {
             super(sensorAddress, sensorName);
             this.wheelRevolutionsCount = wheelRevolutionsCount;
             this.wheelRevolutionsTime = wheelRevolutionsTime;
@@ -123,7 +139,8 @@ public final class SensorDataCycling {
 
         public void compute(DistanceSpeed previous, Distance wheelCircumference) {
             if (hasData() && previous != null && previous.hasData()) {
-                float timeDiff_ms = UintUtils.diff(wheelRevolutionsTime, previous.wheelRevolutionsTime, UintUtils.UINT16_MAX) / 1024f * UnitConversions.S_TO_MS;
+                float timeDiff_ms = UintUtils.diff(wheelRevolutionsTime, previous.wheelRevolutionsTime,
+                        UintUtils.UINT16_MAX) / 1024f * UnitConversions.S_TO_MS;
                 Duration timeDiff = Duration.ofMillis((long) timeDiff_ms);
                 if (timeDiff.isZero() || timeDiff.isNegative()) {
                     Log.e(TAG, "Timestamps difference is invalid: cannot compute speed.");
@@ -131,7 +148,8 @@ public final class SensorDataCycling {
                     return;
                 }
 
-                long wheelDiff = UintUtils.diff(wheelRevolutionsCount, previous.wheelRevolutionsCount, UintUtils.UINT32_MAX);
+                long wheelDiff = UintUtils.diff(wheelRevolutionsCount, previous.wheelRevolutionsCount,
+                        UintUtils.UINT32_MAX);
 
                 Distance distance = wheelCircumference.multipliedBy(wheelDiff);
                 Distance distanceOverall = distance;
@@ -153,19 +171,22 @@ public final class SensorDataCycling {
         @NonNull
         @Override
         public String toString() {
-            return super.toString() + " data=" + value + " time=" + wheelRevolutionsTime + " count=" + wheelRevolutionsCount;
+            return super.toString() + " data=" + value + " time=" + wheelRevolutionsTime + " count="
+                    + wheelRevolutionsCount;
         }
 
         @Override
         public boolean equals(@Nullable Object obj) {
-            if (!(obj instanceof DistanceSpeed)) return false;
+            if (!(obj instanceof DistanceSpeed))
+                return false;
 
             DistanceSpeed comp = (DistanceSpeed) obj;
             if (!(hasData() && comp.hasData())) {
                 return false;
             }
 
-            return getWheelRevolutionsCount() == comp.getWheelRevolutionsCount() && getWheelRevolutionsTime() == comp.getWheelRevolutionsTime();
+            return getWheelRevolutionsCount() == comp.getWheelRevolutionsCount()
+                    && getWheelRevolutionsTime() == comp.getWheelRevolutionsTime();
         }
 
         public static class Data {
@@ -181,18 +202,16 @@ public final class SensorDataCycling {
 
             @Override
             public String toString() {
-                return "Data{" +
-                        "distance_m=" + distance +
-                        ", distance_overall_m=" + distanceOverall +
-                        ", speed_mps=" + speed +
-                        '}';
+                return "Data{" + "distance_m=" + distance + ", distance_overall_m=" + distanceOverall + ", speed_mps="
+                        + speed + '}';
             }
         }
     }
 
     public static class CadenceAndSpeed extends SensorData<Pair<Cadence, DistanceSpeed>> {
 
-        public CadenceAndSpeed(String sensorAddress, String sensorName, @Nullable Cadence cadence, @Nullable DistanceSpeed distanceSpeed) {
+        public CadenceAndSpeed(String sensorAddress, String sensorName, @Nullable Cadence cadence,
+                @Nullable DistanceSpeed distanceSpeed) {
             super(sensorAddress, sensorName);
             this.value = new Pair<>(cadence, distanceSpeed);
         }
@@ -206,4 +225,3 @@ public final class SensorDataCycling {
         }
     }
 }
-
--- src/main/java/de/dennisguse/opentracks/util/BluetoothUtils.java
+++ src/main/java/de/dennisguse/opentracks/util/BluetoothUtils.java
@@ -42,17 +42,20 @@ public class BluetoothUtils {
     public static final UUID HEART_RATE_SERVICE_UUID = new UUID(0x180D00001000L, 0x800000805f9b34fbL);
     public static final UUID HEART_RATE_MEASUREMENT_CHAR_UUID = new UUID(0x2A3700001000L, 0x800000805f9b34fbL);
 
-    public static final List<UUID> HEART_RATE_SUPPORTING_DEVICES = Collections.unmodifiableList(Arrays.asList(
-            BluetoothUtils.HEART_RATE_SERVICE_UUID,
-            //Devices that support HEART_RATE_SERVICE_UUID, but do not announce HEART_RATE_SERVICE_UUID in there BLE announcement messages (during device discovery).
-            UUID.fromString("0000fee0-0000-1000-8000-00805f9b34fb") //Miband3
-    ));
+    public static final List<UUID> HEART_RATE_SUPPORTING_DEVICES = Collections
+            .unmodifiableList(Arrays.asList(BluetoothUtils.HEART_RATE_SERVICE_UUID,
+                    // Devices that support HEART_RATE_SERVICE_UUID, but do not announce
+                    // HEART_RATE_SERVICE_UUID in their BLE announcement messages (during device
+                    // discovery).
+                    UUID.fromString("0000fee0-0000-1000-8000-00805f9b34fb") // Miband3
+            ));
 
     public static final UUID CYCLING_POWER_UUID = new UUID(0x181800001000L, 0x800000805f9b34fbL);
     public static final UUID CYCLING_POWER_MEASUREMENT_CHAR_UUID = new UUID(0x2A6300001000L, 0x800000805f9b34fbL);
 
     public static final UUID CYCLING_SPEED_CADENCE_SERVICE_UUID = new UUID(0x181600001000L, 0x800000805f9b34fbL);
-    public static final UUID CYCLING_SPEED_CADENCE_MEASUREMENT_CHAR_UUID = new UUID(0x2A5B00001000L, 0x800000805f9b34fbL);
+    public static final UUID CYCLING_SPEED_CADENCE_MEASUREMENT_CHAR_UUID = new UUID(0x2A5B00001000L,
+            0x800000805f9b34fbL);
 
     private static final String TAG = BluetoothUtils.class.getSimpleName();
 
@@ -74,7 +77,8 @@ public class BluetoothUtils {
     }
 
     public static Integer parseHeartRate(BluetoothGattCharacteristic characteristic) {
-        //DOCUMENTATION https://www.bluetooth.com/wp-content/uploads/Sitecore-Media-Library/Gatt/Xml/Characteristics/org.bluetooth.characteristic.heart_rate_measurement.xml
+        // DOCUMENTATION
+        // https://www.bluetooth.com/wp-content/uploads/Sitecore-Media-Library/Gatt/Xml/Characteristics/org.bluetooth.characteristic.heart_rate_measurement.xml
         byte[] raw = characteristic.getValue();
         if (raw.length == 0) {
             return null;
@@ -92,7 +96,8 @@ public class BluetoothUtils {
     }
 
     public static Integer parseCyclingPower(BluetoothGattCharacteristic characteristic) {
-        // DOCUMENTATION https://www.bluetooth.com/wp-content/uploads/Sitecore-Media-Library/Gatt/Xml/Characteristics/org.bluetooth.characteristic.cycling_power_measurement.xml
+        // DOCUMENTATION
+        // https://www.bluetooth.com/wp-content/uploads/Sitecore-Media-Library/Gatt/Xml/Characteristics/org.bluetooth.characteristic.cycling_power_measurement.xml
         int valueLength = characteristic.getValue().length;
         if (valueLength < 4) {
             return null;
@@ -102,36 +107,41 @@ public class BluetoothUtils {
     }
 
     /**
-     * Documentation: https://www.bluetooth.org/docman/handlers/downloaddoc.ashx?doc_id=261449
+     * Documentation:
+     * https://www.bluetooth.org/docman/handlers/downloaddoc.ashx?doc_id=261449
      */
-    public static SensorDataCycling.CadenceAndSpeed parseCyclingCrankAndWheel(String address, String sensorName, @NonNull BluetoothGattCharacteristic characteristic) {
-        // DOCUMENTATION https://www.bluetooth.com/wp-content/uploads/Sitecore-Media-Library/Gatt/Xml/Characteristics/org.bluetooth.characteristic.csc_measurement.xml
+    public static SensorDataCycling.CadenceAndSpeed parseCyclingCrankAndWheel(String address, String sensorName,
+            @NonNull BluetoothGattCharacteristic characteristic) {
+        // DOCUMENTATION
+        // https://www.bluetooth.com/wp-content/uploads/Sitecore-Media-Library/Gatt/Xml/Characteristics/org.bluetooth.characteristic.csc_measurement.xml
         int valueLength = characteristic.getValue().length;
         if (valueLength == 0) {
             return null;
         }
 
         int flags = characteristic.getValue()[0];
-        boolean hasCrank = (flags & 0x01) > 0;
-        boolean hasWheel = (flags & 0x02) > 0;
+        boolean hasWheel = (flags & 0x01) > 0;
+        boolean hasCrank = (flags & 0x02) > 0;
+        int index = 1;
+
+        SensorDataCycling.DistanceSpeed speed = null;
+        if (hasWheel && valueLength - index >= 6) {
+            long wheelCount = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT32, index);
+            index += 4;
+
+            int wheelTime = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT16, index); // 1/1024s
+            index += 2;
+            speed = new SensorDataCycling.DistanceSpeed(address, sensorName, wheelCount, wheelTime);
+        }
 
         SensorDataCycling.Cadence cadence = null;
-        int index = 1;
-        if (hasCrank && valueLength - index >= 6) {
-            long crankCount = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT32, index);
-            index += 4;
+        if (hasCrank && valueLength - index >= 4) {
+            int crankCount = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT16, index);
+            index += 2;
 
             int crankTime = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT16, index); // 1/1024s
             index += 2;
             cadence = new SensorDataCycling.Cadence(address, sensorName, crankCount, crankTime);
-        }
-
-        SensorDataCycling.DistanceSpeed speed = null;
-        if (hasWheel && valueLength - index >= 4) {
-            int wheelCount = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT16, index);
-            index += 2;
-            int wheelTime = characteristic.getIntValue(BluetoothGattCharacteristic.FORMAT_UINT16, index); // 1/1024s
-            speed = new SensorDataCycling.DistanceSpeed(address, sensorName, wheelCount, wheelTime);
         }
 
         return new SensorDataCycling.CadenceAndSpeed(address, sensorName, cadence, speed);
